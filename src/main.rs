9f2f8f4a198a76a5a74dfc60ab26c0358f0938e2
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-02-18 22:06:42 +0800

    array

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,32 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    //match with cmp
-    let my_age = 18;
-    let voting_age = 18;
-    match my_age.cmp(&voting_age){
-        Ordering::Less => println!("Can't Vote"),
-        Ordering::Greater => println!("Can Vote"),
-        Ordering::Equal => println!("You gained the right to vote"),
-    };
+    let arr_1 = [1,2,3,4];
+    println!("1st: {}", arr_1[0]);
+    println!("Length: {}", arr_1.len());
 
-    //match statement
-    // let age2 = 8;
-    // match age2 {
-    //     1..=18 => println!("Important Birthday"),
-    //     21 | 50 => println!("Important Birthday"),
-    //     65..=i32::MAX => println!("Important Birthday"),
-    //     _ => println!("Not an important Birthday"),
-    // };
-    // let age = 8;
-    // if (age >= 1) && (age <= 18) {
-    //     println!("Important Birthday");
-    // } else if (age == 21) || (age == 50){
-    //     println!("Important Birthday");
-    // } else if age >= 65 {
-    //     println!("Important Birthday");
-    // } else {
-    //     println!("Not an important birthday");
+    //loop
+    let arr_2 = [1,2,3,4,5,6,7,8,9];
+    let mut loop_idx = 0;
+
+    for val in arr_2.iter() {
+        println!("Val : {}", val);
+    }
+    //while loop
+    // while loop_idx < arr_2.len(){
+    //     println!("Arr : {}", arr_2[loop_idx]);
+    //     loop_idx += 1;
     // }
 
-    //ternary operator
-    // let mut my_age = 47;
-    // let can_vote = if my_age >= 18 {
-    //     true
-    // } else{
-    //     false
-    // };
-    // println!("Can Vote: {}", can_vote)
-    
+    // loop {
+    //     if arr_2[loop_idx] % 2 == 0 {
+    //         loop_idx += 1;
+    //         continue;
+    //     }
+    //     if arr_2[loop_idx] == 9 {
+    //         break;
+    //     }
+    //     println!("Val : {}", arr_2[loop_idx]);
+    //     loop_idx += 1;
+    // }
 }
