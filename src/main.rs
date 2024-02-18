5a180e2ad924f7dc22946177dbf3c69f99aa42c2
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-02-18 21:33:24 +0800

    if statements and match

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,41 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let num_1: f32 = 1.111111111111111;
-    println!("f:32 : {}", num_1 + 0.111111111111111);
-    let num_2: f64 = 1.111111111111111;
-    println!("f:64 : {}", num_2 + 0.111111111111111);
-    let num_3: u32 = 5;
-    let num_4: u32 = 4;
-    println!("5 + 4 = {}", num_3+num_4);
-    println!("5 - 4 = {}", num_3-num_4);
-    println!("5 * 4 = {}", num_3*num_4);
-    println!("5 / 4 = {}", num_3/num_4);
-    println!("5 % 4 = {}", num_3%num_4);
+    //match with cmp
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age){
+        Ordering::Less => println!("Can't Vote"),
+        Ordering::Greater => println!("Can Vote"),
+        Ordering::Equal => println!("You gained the right to vote"),
+    };
 
-    let random_num = rand::thread_rng().gen_range(1..101);
-    println!("Random: {}", random_num);
+    //match statement
+    // let age2 = 8;
+    // match age2 {
+    //     1..=18 => println!("Important Birthday"),
+    //     21 | 50 => println!("Important Birthday"),
+    //     65..=i32::MAX => println!("Important Birthday"),
+    //     _ => println!("Not an important Birthday"),
+    // };
+    // let age = 8;
+    // if (age >= 1) && (age <= 18) {
+    //     println!("Important Birthday");
+    // } else if (age == 21) || (age == 50){
+    //     println!("Important Birthday");
+    // } else if age >= 65 {
+    //     println!("Important Birthday");
+    // } else {
+    //     println!("Not an important birthday");
+    // }
+
+    //ternary operator
+    // let mut my_age = 47;
+    // let can_vote = if my_age >= 18 {
+    //     true
+    // } else{
+    //     false
+    // };
+    // println!("Can Vote: {}", can_vote)
+    
 }
