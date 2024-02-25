9d369deb76cbe0a4fdc898352613cba3dfcae1a8
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-02-25 21:05:04 +0800

    struct

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,26 @@ use rand::Rng;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
-use std::collections::HashMap;
-
 
 fn main() {
-   let mut heroes = HashMap::new();
-   heroes.insert("Superman", "Clark Kent");
-   heroes.insert("Batman", "Bruce Wayne");
-   heroes.insert("Flash", "Barry Allen");
-   for(k,v) in heroes.iter(){
-    println!("{} = {}", k, v)
-   }
-   if heroes.contains_key("Batman"){
-    let the_batman = heroes.get("Batman");
-    match the_batman {
-        Some(x) => println!("Batman is a hero"),
-        None => println!("Batman is not a hero"),
+//    struct Customer{
+//     name: String,
+//     address: String,
+//     balance: f32,
+//    }
+
+//    let mut bob = Customer{
+//     name: String::from("Bob Smith"),
+//     address: String::from("555 Main St"),
+//     balance : 234.50
+//    };
+
+//    bob.address = String::from("505 main St");
+    struct Rectangle<T, U> {
+        length: T,
+        height: U,
     }
-   }
-   //println!("Length : {}", heroes.len())
+
+    let rec = Rectangle{length: 4, height:10.5};
+
 }
