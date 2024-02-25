dde35969a293deb7a87bae83ca85b951d3d40971
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-02-25 20:47:54 +0800

    generics

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,13 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
-fn sum_list(list: &[i32]) -> i32 {
-    let mut sum = 0;
-    for &val in list.iter() {
-        sum += &val;
-    }
-    sum
+use std::ops::Add;
+
+fn get_sum_gen<T:Add<Output = T>>(x: T, y: T) -> T {
+    return x + y;
 }
-// fn get_2(x:i32) -> (i32,i32){
-//     return (x+1, x+2);
-// }
+
 fn main() {
-    let num_list = vec![1,2,3,4,5];
-    println!("Sum of list = {}", sum_list(&num_list));
-    // let (val_1, val_2) = get_2(3);
-    // println!("Nums: {} {}", val_1,val_2);
+   println!("5 + 4 = {}", get_sum_gen(5, 4));
+   println!("5.2 + 4.6 = {}", get_sum_gen(5.2, 4.6));
 }
