5f4cc90b93b16299449873532fb058339f62208f
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2024-02-26 19:29:09 +0800

    closures 2

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,25 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let mut arr_it = [1,2,3,4];
-    for val in arr_it.iter(){
-        println!("{}",val);
+    // let var_name = |parameters|  -> return_type {body}
+    // let can_vote = |age: i32| {
+    //     age >= 18
+    // };
+    // println!("Can vote: {}", can_vote(8));
+    // let mut samp1 = 5;
+    // let print_var = || println!("samp1 = {}", samp1);
+    // print_var();
+    // samp1 = 10;
+    // let mut change_var = || samp1 += 1;
+    // change_var();
+    // println!("samp1 = {}", samp1);
+    // samp1 = 10;
+    // println!("samp1 = {}", samp1);
+    fn use_func<T>(a: i32, b:i32, func: T) -> i32 where T: Fn(i32,i32) -> i32 {
+        func(a,b)
     }
-    //arr_it.into_iter();
-    let mut iter1 = arr_it.iter();
-    println!("1st : {:?}",iter1.next());
+    let sum = |a,b| a+b;
+    let prod = |a,b| a*b;
+    println!("5 + 4 = {}", use_func(5, 4, sum));
+    println!("5 * 4 = {}", use_func(5, 4, prod));
 }
